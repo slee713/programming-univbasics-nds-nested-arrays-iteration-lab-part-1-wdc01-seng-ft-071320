92cadc4aefc61e89f855e8fa07eebaef2b294b5e
Steven Lee <steven.lee1393@gmail.com>
2020-07-01 23:43:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,7 +11,7 @@ def find_even_values(src)
     while inside_count < src[outside_count].length do
       if src[outside_count][inside_count] % 2 == 0
         even_values.push(src[outside_count][inside_count])
-        inside_count += 1 
+        inside_count += 1
       end
     end
     outside_count += 1
